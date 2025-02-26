52e111670f6f1dfbf9455e0f6c3dd722743b095a
nikolas <nikolassc4mpos@gmail.com>
2025-02-26 13:50:43 -0300

    add infra

--- src/bot/gitbot.js
+++ src/bot/gitbot.js
@@ -1,4 +1,4 @@
-  require('dotenv').config();
+  require('dotenv').config({ path: './config/.env' });
   const express = require('express');
   const bodyParser = require('body-parser');
   const axios = require('axios');
